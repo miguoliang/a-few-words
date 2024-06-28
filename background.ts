f42173a465a0c255d5031a9ef7927966feda5ca4
Mi Guoliang <boymgl@qq.com>
2024-06-28 17:15:56 +0800

    feat: Add context menu to save selected text to A few words
    This commit adds a new context menu item to the Plasmo extension called "Save to A few words". When the user selects text on a webpage and right-clicks, they will see this option in the context menu. Clicking on it will save the selected text to the extension's local storage.
    
    The necessary permissions for the context menu functionality have also been added to the manifest file.

---
+++ background.ts
@@ -0,0 +1,30 @@
+export {}
+
+chrome.runtime.onInstalled.addListener(async () => {
+  chrome.contextMenus.create({
+    id: "getSelectedText",
+    title: "Save to A few words",
+    contexts: ["selection"]
+  })
+})
+
+// Open a new search tab when the user clicks a context menu
+chrome.contextMenus.onClicked.addListener((item, tab) => {
+  const tld = item.menuItemId
+  if (tld === "getSelectedText") {
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tab.id },
+        func: () => {
+          return window.getSelection()?.toString()
+        }
+      },
+      (selectedText) => {
+        const text = selectedText[0].result
+        chrome.storage.local.set({ text: text }, () => {
+          console.log("Text is set to " + text)
+        })
+      }
+    )
+  }
+})
