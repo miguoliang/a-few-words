66b33abdbcb6db1b0892f96b932bf7369ce08471
Mi Guoliang <boymgl@qq.com>
2024-07-01 17:49:22 +0800

    feat: Display words like a card

--- sidepanel.tsx
+++ sidepanel.tsx
@@ -1,17 +1,33 @@
 import { useStorage } from "@plasmohq/storage/hook"
 
-import type { CardList } from "~content"
+import "~style.css"
+
+import type { CardList, Card as CardType } from "~content"
 
 const SidePanel = () => {
   const [cards] = useStorage<CardList>("cards", [])
   return (
-    <div>
+    <ul className="p-2 gap-2 flex flex-col">
       {cards.map((card, index) => (
-        <div key={index}>
-          <div>{card.front}</div>
-          <div>{card.back}</div>
-        </div>
+        <li key={index}>
+          <Card card={card} />
+        </li>
       ))}
+    </ul>
+  )
+}
+
+const Card = ({ card }: { card: CardType }) => {
+  return (
+    <div className="card bg-neutral text-neutral-content w-96">
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{card.front}</h2>
+        <p>{card.back}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Easy</button>
+          <button className="btn btn-ghost">Hard</button>
+        </div>
+      </div>
     </div>
   )
 }
