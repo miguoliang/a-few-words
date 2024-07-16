2e02e9b6f4addea7e44ffbfd71e0d97afb69c3e2
Mi Guoliang <boymgl@qq.com>
2024-07-16 08:22:54 +0800

    chore: Add Secrets.toml file and update dependencies in Cargo.toml

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,39 +1,35 @@
-use std::io;
-
 use actix_web::{
     error,
-    web::{self, Data},
-    App, HttpServer, Responder,
+    web::{self, Data, ServiceConfig},
+    Responder,
 };
 use diesel::{r2d2, PgConnection, RunQueryDsl, SelectableHelper};
-use dotenvy::dotenv;
 use models::{NewWord, Word};
+use shuttle_actix_web::ShuttleActixWeb;
 
 mod models;
 mod schema;
 
 type DbPool = r2d2::Pool<r2d2::ConnectionManager<PgConnection>>;
 
-#[actix_web::main]
-async fn main() -> io::Result<()> {
-    dotenv().ok();
-    // connect to PostgreSQL database
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let manager = r2d2::ConnectionManager::<PgConnection>::new(database_url);
-    let pool = r2d2::Pool::builder()
-        .build(manager)
-        .expect("database URL should be valid path to SQLite DB file");
+#[shuttle_runtime::main]
+async fn main(
+    #[shuttle_shared_db::Postgres(
+        local_uri = "postgres://username:{secrets.PASSWORD}@localhost:5432/a-few-words"
+    )] conn_str: String,
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let app_config = move |cfg: &mut ServiceConfig| {
+        // connect to PostgreSQL database
+        let manager = r2d2::ConnectionManager::<PgConnection>::new(conn_str);
+        let pool = r2d2::Pool::builder()
+            .build(manager)
+            .expect("database URL should be valid path to SQLite DB file");
 
-    // start HTTP server on port 8080
-    HttpServer::new(move || {
-        App::new()
-            .app_data(Data::new(pool.clone()))
+        cfg.app_data(Data::new(pool.clone()))
             .route("/words", web::get().to(get_words))
-            .route("/words", web::post().to(create_word))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+            .route("/words", web::post().to(create_word));
+    };
+    Ok(app_config.into())
 }
 
 async fn get_words(pool: Data<DbPool>) -> actix_web::Result<impl Responder> {
