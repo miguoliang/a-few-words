c128741db8e2ccd28acbd63fa35a1da0bc01a38a
Mi Guoliang <boymgl@qq.com>
2024-08-25 09:25:22 +0800

    chore: use environment variable in chrome extension

--- chrome-extension/content.ts
+++ chrome-extension/content.ts
@@ -94,7 +94,7 @@ export const fetchWords = async (
 ) => {
   try {
     const response = await fetch(
-      `http://localhost:8000/api/v1/words?offset=${offset}&size=${size}`,
+      `http://${API_HOST}/api/v1/words?offset=${offset}&size=${size}`,
       {
         headers: {
           Authorization: `Bearer ${store.getState().auth.access_token}`
