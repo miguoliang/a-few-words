52805da0a08a6dcc9f21a58b7fee30d3b89b31b3
Mi Guoliang <boymgl@qq.com>
2024-09-08 14:45:54 +0800

    chore: update website login page

--- chrome-extension/WelcomeView.tsx
+++ chrome-extension/WelcomeView.tsx
@@ -14,7 +14,7 @@ const WelcomeView = () => {
 
   return (
     <div className="flex flex-col items-stretch justify-center gap-8 px-8 -mt-16 h-[100vh]">
-      <div className="overflow-hidden h-[200px] rounded-3xl border-[5px] border-black border-solid">
+      <div className="overflow-hidden h-[200px] w-[300px] mx-auto rounded-3xl border-[5px] border-black border-solid">
         <img src={banner} alt="logo" className="mt-[-30px]" />
       </div>
       <button
