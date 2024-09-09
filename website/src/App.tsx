87d1436cc0feda114e08aa0bffa1ec57ce18aaa8
Mi Guoliang <boymgl@qq.com>
2024-09-09 23:25:08 +0800

    chore: update landing page of users being signed in

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,7 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return <h1>Hello Vite + React!</h1>
+  return (
+    <div className="flex flex-col items-stretch justify-center gap-8 px-8 -mt-16 h-[100vh]">
+      <div className="overflow-hidden h-[200px] w-[300px] rounded-3xl border-[5px] border-black border-solid mx-auto">
+        <img src="public/banner.webp" alt="logo" className="mt-[-30px]" />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
