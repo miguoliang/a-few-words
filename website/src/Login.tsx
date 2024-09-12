f1dbb9fe202be17e19e6658ac3489b0169ebcd41
Mi Guoliang <boymgl@qq.com>
2024-09-12 21:49:08 +0800

    refactor: separate logo into a component

--- website/src/Login.tsx
+++ website/src/Login.tsx
@@ -1,11 +1,10 @@
+import Logo from "./Logo";
 import { userManager } from "./oidc";
 
 const Login = () => {
   return (
     <div className="flex flex-col items-stretch justify-center gap-8 px-8 -mt-16 h-[100vh]">
-      <div className="overflow-hidden h-[200px] w-[300px] rounded-3xl border-[5px] border-black border-solid mx-auto">
-        <img src="public/banner.webp" alt="logo" className="mt-[-30px]" />
-      </div>
+      <Logo />
       <button
         onClick={() => {
           userManager.signinRedirect();
---
+++ website/src/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <div className="overflow-hidden h-[200px] w-[300px] rounded-3xl border-[5px] border-black border-solid mx-auto">
+      <img src="public/banner.webp" alt="logo" className="mt-[-30px]" />
+    </div>
+  );
+}
